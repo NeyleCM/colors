e39933842d9487cfd7db7a72d9564a6782c90893
NeyleCM <neylecmorantes@gmail.com>
2024-11-18 19:54:12 +0100

    useRef

--- src/components/MyFormChallenge.jsx
+++ src/components/MyFormChallenge.jsx
@@ -1,37 +1,39 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import BoxColor from './BoxColor';
 
 const colors = ['red', 'green', 'pink', 'yellow', 'purple', 'white', 'blue', 'aqua', 'olive'];
 
 const MyForm = () => {
   const [inputValue, setInputValue] = useState('');
-  const boxesRef = useRef([]);
+  const inputRef = useRef(null); 
+
+  useEffect(() => {
+    inputRef.current.focus(); 
+  }, []);
 
   const handleChange = (e) => {
     setInputValue(e.target.value.toLowerCase());
   };
 
   return (
-    <>
-      <div>
-        <input
-          type="text"
-          placeholder="Escribe un color..."
-          value={inputValue}
-          onChange={handleChange}
-        />
-        <div className="box-container">
-          {colors.map((color, index) => (
-            <BoxColor
-              key={color}
-              color={color}
-              isActive={inputValue === color}
-            />
-          ))}
-        </div>
+    <div>
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder="Escribe un color..."
+        value={inputValue}
+        onChange={handleChange}
+      />
+      <div className="box-container">
+        {colors.map((color) => (
+          <BoxColor
+            key={color}
+            color={color}
+            isActive={inputValue === color}
+          />
+        ))}
       </div>
-    </>
+    </div>
   );
 };
-
 export default MyForm;
